2528b345b9e5bd8d7eef69e861cdb4f8c97d845a
dhanish-00 <m.dhanish.np@gmail.com>
2025-10-21 15:11:36 +0530

    read file completed

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"log"
+	"fmt"
+)
+
+
+func main() {
+	file, err := os.Open("messages.txt")
+
+	if err != nil {
+		log.Fatal("error",err)
+	}
+	
+	for {
+		data := make([]byte,8)
+		n, err := file.Read(data)
+
+		if err!= nil {
+			break
+		}
+		
+		fmt.Printf("read %s \n",string(data[:n]))
+	}
+	
+
+}

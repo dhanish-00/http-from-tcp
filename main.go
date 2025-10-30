d4d1bc9a3393a5a55b4785a245789aa381286157
dhanish-00 <m.dhanish.np@gmail.com>
2025-10-30 15:21:47 +0530

    added concurrent reading

--- main.go
+++ main.go
@@ -5,8 +5,39 @@ import (
 	"log"
 	"fmt"
 	"bytes"
+	"io"
 )
 
+func getLinesChannel(file io.ReadCloser ) <-chan string{
+	out := make(chan string,1)
+	go func(){
+		defer file.Close()
+		defer close(out)
+		
+			str := ""	
+		for {
+			data := make([]byte,8)
+			n, err := file.Read(data)
+
+			if err!= nil {
+				break
+			}
+			data = data[:n]
+			
+			if i := bytes.IndexByte(data, '\n'); i!=-1{
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+			str += string(data)
+		}
+		if len(str) != 0{
+			out <- str 
+		}
+	}()
+	return out
+}
 
 func main() {
 	file, err := os.Open("messages.txt")
@@ -14,26 +45,9 @@ func main() {
 	if err != nil {
 		log.Fatal("error",err)
 	}
-	str := ""	
-	for {
-		data := make([]byte,8)
-		n, err := file.Read(data)
-
-		if err!= nil {
-			break
-		}
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i!=-1{
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read:%s\n",str)
-			str = ""
-		}
-		str += string(data)
+	lines := getLinesChannel(file)
+	for line := range lines{
+		fmt.Printf("read: %s \n", line)
 	}
-	if len(str) != 0{
-		fmt.Printf("read:%s\n",str)
-	}
-	
 
 }
